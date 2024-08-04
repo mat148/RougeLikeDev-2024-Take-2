c693feaa67adfc7767983611e0b5961bbe2a8a34
Matthew Morse <mmorse@platformq.com>
2024-08-04 00:45:34 -0400

    random roads test

--- src/Game/road-test.gd
+++ src/Game/road-test.gd
@@ -4,13 +4,18 @@ extends Node2D
 
 @export var world_width: int = 60
 @export var world_height: int = 60
+var world_rect
 
 @export var tile_size: int = 16
 var total_unit_size: int = 448
+var rect_width: int = 224
 
 @export var num_of_roads: int = 30
 
-var roads: Array[Line2D]
+var roads: Array[Rect2]
+
+#Generate a random plot rect2
+
 
 #2 tiles for walkable sidewalk
 #1 tile for sidewalk accessories
@@ -24,7 +29,7 @@ func _ready() -> void:
 	world_width = ((world_width * tile_size / total_unit_size) * total_unit_size)
 	world_height = ((world_height * tile_size / total_unit_size) * total_unit_size)
 	
-	var world_rect: Rect2 = Rect2(Vector2(0,0), Vector2(world_width, world_height) * tile_size)
+	world_rect = Rect2(Vector2(0,0), Vector2(world_width, world_height) * tile_size)
 	
 	var world_line = Line2D.new()
 	world_line.width = 1
@@ -39,6 +44,7 @@ func _ready() -> void:
 	for x in range(0, world_rect.size.x, tile_size):
 		var line = Line2D.new()
 		line.width = 1
+		line.default_color = Color.CHARTREUSE
 		line.add_point(Vector2(x, 0))
 		line.add_point(Vector2(x, world_rect.size.y))
 		add_child(line)
@@ -56,21 +62,31 @@ func _ready() -> void:
 	#plots 14 tiles = 224
 	#4800 / 448 = must be even
 	
-	for x in range(7 * tile_size, world_rect.size.x, (tile_size * 14) * 2):
-		var line = Line2D.new()
-		line.width = tile_size * 14
-		line.add_point(Vector2(x, 0))
-		line.add_point(Vector2(x, world_rect.size.y))
-		add_child(line)
-		roads.append(line)
+	for i in num_of_roads:
+		var road: Area2D = Area2D.new()
+		var road_collision: CollisionPolygon2D = CollisionPolygon2D.new()
+		
+		var points = generate_rectangles()
+		
+		road_collision.polygon = points
+		road.add_child(road_collision)
+		add_child(road)
 	
-	for y in range(7 * tile_size, world_rect.size.y, (tile_size * 14) * 2):
-		var line = Line2D.new()
-		line.width = tile_size * 14
-		line.add_point(Vector2(0, y))
-		line.add_point(Vector2(world_rect.size.x, y))
-		add_child(line)
-		roads.append(line)
+	#for x in range(0, world_rect.size.x, (tile_size * 14) * 2):
+		#var road: Rect2 = Rect2(Vector2(x, 0), Vector2(tile_size * 14, world_rect.size.y))
+		#roads.append(road)
+	#
+	#for y in range(0, world_rect.size.y, (tile_size * 14) * 2):
+		#var road: Rect2 = Rect2(Vector2(0, y), Vector2(world_rect.size.x, tile_size * 14))
+		#roads.append(road)
+	
+	#for y in range(7 * tile_size, world_rect.size.y, (tile_size * 14) * 2):
+		#var line = Line2D.new()
+		#line.width = tile_size * 14
+		#line.add_point(Vector2(0, y))
+		#line.add_point(Vector2(world_rect.size.x, y))
+		#add_child(line)
+		#roads.append(line)
 	
 	#for y in range(0, world_rect.size.y, tile_size * 14):
 		#var line = Line2D.new()
@@ -90,20 +106,33 @@ func _ready() -> void:
 
 func _process(delta: float) -> void:
 	if Input.is_action_just_pressed("ui_accept"):
-		var road1 = roads[randi_range(0, roads.size() - 1)]
-		var road2 = roads[randi_range(0, roads.size() - 1)]
-		road1.default_color = Color.BLUE
-		road2.default_color = Color.BLUE
+		var road1: Rect2 = roads.pick_random()
+		var road2: Rect2 = roads.pick_random()
 		
-		print(road1, road2)
-		print(get_line_intersection(road1, road2))
+		print(road1.intersects(road2))
+		#var overlapping_roads = road.get_overlapping_areas()
+		#print(overlapping_roads)
+		#
+		#test_areas(road, overlapping_roads[0])
+		
+		#var road1 = roads[randi_range(0, roads.size() - 1)]
+		#var road2 = roads[randi_range(0, roads.size() - 1)]
+		#road1.default_color = Color.BLUE
+		#road2.default_color = Color.BLUE
+		#
+		#print(road1, road2)
+		#print(get_line_intersection(road1, road2))
 
 func get_random_point_on_rect_edge(rect: Rect2) -> Array[Vector2]:
-	var points: Array[Vector2] = []
+	var points: Array[Vector2]
 	var previous_edge = null
 	
 	for point in 2:
+		var x: float
+		var y: float
+		
 		var random_edge
+		
 		if !previous_edge:
 			random_edge = randi() % 4  # Randomly select one of the four edges
 			previous_edge = random_edge
@@ -117,9 +146,6 @@ func get_random_point_on_rect_edge(rect: Rect2) -> Array[Vector2]:
 					random_edge = 3
 				3:
 					random_edge = 2
-		
-		var x: float
-		var y: float
 
 		match random_edge:
 			0:
@@ -165,3 +191,42 @@ func get_line_intersection(line1: Line2D, line2: Line2D) -> Vector2:
 		return Vector2(p1.x + (t * s1_x), p1.y + (t * s1_y))
 
 	return Vector2.INF
+
+func test_areas(area1: Area2D, area2: Area2D) -> void:
+	var area1_polygon = area1.get_child(0).polygon
+	var area2_polygon = area2.get_child(0).polygon
+	
+	print(area1_polygon)
+	print(area2_polygon)
+
+func generate_rectangles() -> Array:
+	var rect_type = randi() % 3  # 0: parallel, 1: perpendicular, 2: diagonal
+	var variation = randf_range(-10, 10)  # Slight variation in position
+	
+	var angle = 0.0
+	if rect_type == 0:
+		# Rectangle parallel to the x-axis
+		var start_x = randf() * (world_rect.size.x - rect_width)
+		var start_y = randf() * (world_rect.size.y - world_rect.size.y)
+		return generate_parallel_rectangle(Vector2(start_x + variation, start_y))
+	else:
+		# Rectangle perpendicular to the x-axis
+		var start_x = randf() * (world_rect.size.x - world_rect.size.x)
+		var start_y = randf() * (world_rect.size.y - rect_width)
+		return generate_perpendicular_rectangle(Vector2(start_x, start_y + variation))
+
+func generate_parallel_rectangle(start: Vector2) -> Array:
+	return [
+		start,
+		start + Vector2(rect_width, 0),
+		start + Vector2(rect_width, world_rect.size.y),
+		start + Vector2(0, world_rect.size.y)
+	]
+
+func generate_perpendicular_rectangle(start: Vector2) -> Array:
+	return [
+		start,
+		start + Vector2(world_rect.size.x, 0),
+		start + Vector2(world_rect.size.x, rect_width),
+		start + Vector2(0, rect_width)
+	]
