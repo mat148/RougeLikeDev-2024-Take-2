6cc9ca48f514049128abd1d892a929dc3e41f60e
Matthew Morse <mmorse@platformq.com>
2024-08-05 01:25:33 -0400

    Multi threading attempt

--- src/Game/wfc-test.gd
+++ src/Game/wfc-test.gd
@@ -1,6 +1,1 @@
 extends Node2D
-
-@onready var map: MapWFC = $Map
-
-func _ready() -> void:
-	map.generate()
--- src/Map/map_wfc.gd
+++ src/Map/map_wfc.gd
@@ -1,11 +1,18 @@
 class_name MapWFC
 extends Node2D
 
+var thread: Thread
+
 @export var fov_radius: int = 8
 
 var map_data: MapDataWFC
 @onready var dungeon_generator: WFC_Generator = $Wfc_generator
 #@onready var field_of_view: FieldOfView = $FieldOfView
+
+func _ready() -> void:
+	thread = Thread.new()
+	thread.start(generate)
+	print(thread.wait_to_finish())
 
 func generate() -> void:
 	map_data = await dungeon_generator.generate_dungeon()
@@ -14,7 +21,7 @@ func generate() -> void:
 func _place_tiles() -> void:
 	for x in range(0, map_data.width, 1):
 		for y in range(0, map_data.height, 1):
-			add_child(map_data.tiles[x][y])
+			call_deferred("add_child", map_data.tiles[x][y])
 
 #func update_fov(player_position: Vector2i) -> void:
 	#field_of_view.update_fov(map_data, player_position, fov_radius)
